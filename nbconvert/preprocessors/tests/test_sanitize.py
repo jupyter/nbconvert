3eff4a4f7bbf388dfb157aa3e6b024cfdb1e4bf1
YuviPanda <yuvipanda@gmail.com>
2015-12-09 01:35:34 -0800

    Add some tests

---
+++ nbconvert/preprocessors/tests/test_sanitize.py
@@ -0,0 +1,117 @@
+"""Tests for the HTMLSanitize preprocessor"""
+
+from .base import PreprocessorTestsBase
+from ..sanitize import SanitizeHTML
+
+
+class TestSanitizer(PreprocessorTestsBase):
+    """Contains test functions for sanitize.py"""
+
+    def build_preprocessor(self):
+        """Make an instance of a preprocessor"""
+        preprocessor = SanitizeHTML()
+        preprocessor.enabled = True
+        return preprocessor
+
+    def preprocess_source(self, cell_type, source, preprocessor):
+        nb = self.build_notebook()
+        res = self.build_resources()
+
+        nb.cells[0].cell_type = cell_type
+        nb.cells[0].source = source
+
+        nb, res = preprocessor(nb, res)
+
+        return nb.cells[0].source
+
+    def test_constructor(self):
+        """Can a SanitizeHTML be constructed?"""
+        self.build_preprocessor()
+
+    def test_tag_whitelist_stripping(self):
+        """Test tag whitelisting + stripping out offending tags"""
+        preprocessor = self.build_preprocessor()
+        preprocessor.strip = True
+
+        self.assertEqual(
+            self.preprocess_source(
+                'markdown',
+                '_A_ <em>few</em> <script>tags</script>',
+                preprocessor
+            ),
+            '_A_ <em>few</em> tags'
+        )
+
+    def test_comment_stripping(self):
+        """Test HTML comment stripping"""
+        preprocessor = self.build_preprocessor()
+
+        self.assertEqual(
+            self.preprocess_source(
+                'markdown',
+                '_A_ <em>few</em> <!-- tags -->',
+                preprocessor
+            ),
+            '_A_ <em>few</em> '
+        )
+
+        preprocessor.strip_comments = False
+        self.assertEqual(
+            self.preprocess_source(
+                'markdown',
+                '_A_ <em>few</em> <!-- tags -->',
+                preprocessor
+            ),
+            '_A_ <em>few</em> <!-- tags -->'
+        )
+
+    def test_attributes_whitelist(self):
+        """Test style"""
+        preprocessor = self.build_preprocessor()
+
+        preprocessor.attributes['a'] = ['href', 'title']
+
+        self.assertEqual(
+            self.preprocess_source(
+                'markdown',
+                '<a href="link" rel="nofollow">Hi</a>',
+                preprocessor
+            ),
+            '<a href="link">Hi</a>'
+        )
+
+    def test_style_whitelist(self):
+        """Test style"""
+        preprocessor = self.build_preprocessor()
+
+        if '*' in preprocessor.attributes:
+            preprocessor.attributes['*'].append('style')
+        else:
+            preprocessor.attributes['*'] = ['style']
+        preprocessor.styles = [
+            'color',
+        ]
+
+        self.assertEqual(
+            self.preprocess_source(
+                'markdown',
+                '_A_ <em style="color: blue; background-color: pink">'
+                'few</em> <script>tags</script>',
+                preprocessor
+            ),
+            '_A_ <em style="color: blue;">few</em> '
+            '&lt;script&gt;tags&lt;/script&gt;'
+        )
+
+    def test_tag_whitelist(self):
+        """Test tag whitelisting"""
+        preprocessor = self.build_preprocessor()
+
+        self.assertEqual(
+            self.preprocess_source(
+                'markdown',
+                '_A_ <em>few</em> <script>tags</script>',
+                preprocessor
+            ),
+            '_A_ <em>few</em> &lt;script&gt;tags&lt;/script&gt;'
+        )
