eb0c9fadf65517dc0066d7b26a8ccb73e63b38b7
Matthias Geier <Matthias.Geier@gmail.com>
2016-02-28 19:14:29 +0100

    Implement 16 named ANSI colors

--- nbconvert/filters/tests/test_ansi.py
+++ nbconvert/filters/tests/test_ansi.py
@@ -35,33 +35,33 @@ class TestAnsi(TestsBase):
         correct_outputs = {
             '\x1b[31m': '',
             'hello\x1b[34m': 'hello',
-            'he\x1b[32m\x1b[36mllo': 'he<span class="ansicyan">llo</span>',
-            '\x1b[1;33mhello': '<span class="ansiyellow ansibold">hello</span>',
-            '\x1b[37mh\x1b[0;037me\x1b[;0037ml\x1b[00;37ml\x1b[;;37mo': '<span class="ansigray">h</span><span class="ansigray">e</span><span class="ansigray">l</span><span class="ansigray">l</span><span class="ansigray">o</span>',
-            'hel\x1b[0;32mlo': 'hel<span class="ansigreen">lo</span>',
+            'he\x1b[32m\x1b[36mllo': 'he<span class="ansi-cyan-fg">llo</span>',
+            '\x1b[1;33mhello': '<span class="ansi-yellow-intense-fg ansi-bold">hello</span>',
+            '\x1b[37mh\x1b[0;037me\x1b[;0037ml\x1b[00;37ml\x1b[;;37mo': '<span class="ansi-white-fg">h</span><span class="ansi-white-fg">e</span><span class="ansi-white-fg">l</span><span class="ansi-white-fg">l</span><span class="ansi-white-fg">o</span>',
+            'hel\x1b[0;32mlo': 'hel<span class="ansi-green-fg">lo</span>',
             'hellø': 'hellø',
-            '\x1b[1mhello\x1b[33mworld\x1b[0m': '<span class="ansibold">hello</span><span class="ansiyellow ansibold">world</span>',
+            '\x1b[1mhello\x1b[33mworld\x1b[0m': '<span class="ansi-bold">hello</span><span class="ansi-yellow-intense-fg ansi-bold">world</span>',
         }
 
         for inval, outval in correct_outputs.items():
-            self.fuzzy_compare(outval, ansi2html(inval))
+            self.assertEqual(outval, ansi2html(inval))
 
     def test_ansi2latex(self):
         """ansi2latex test"""
         correct_outputs = {
             '\x1b[31m': '',
             'hello\x1b[34m': 'hello',
-            'he\x1b[32m\x1b[36mllo': r'he\textcolor{cyan}{llo}',
-            '\x1b[1;33mhello': r'\textcolor{brown}{\textbf{hello}}',
-            '\x1b[37mh\x1b[0;037me\x1b[;0037ml\x1b[00;37ml\x1b[;;37mo': r'\textcolor{lightgray}{h}\textcolor{lightgray}{e}\textcolor{lightgray}{l}\textcolor{lightgray}{l}\textcolor{lightgray}{o}',
-            'hel\x1b[0;32mlo': r'hel\textcolor{green}{lo}',
+            'he\x1b[32m\x1b[36mllo': r'he\textcolor{ansi-cyan}{llo}',
+            '\x1b[1;33mhello': r'\textcolor{ansi-yellow-intense}{\textbf{hello}}',
+            '\x1b[37mh\x1b[0;037me\x1b[;0037ml\x1b[00;37ml\x1b[;;37mo': r'\textcolor{ansi-white}{h}\textcolor{ansi-white}{e}\textcolor{ansi-white}{l}\textcolor{ansi-white}{l}\textcolor{ansi-white}{o}',
+            'hel\x1b[0;32mlo': r'hel\textcolor{ansi-green}{lo}',
             'hello': 'hello',
-            'hello\x1b[34mthere\x1b[mworld': r'hello\textcolor{blue}{there}world',
+            'hello\x1b[34mthere\x1b[mworld': r'hello\textcolor{ansi-blue}{there}world',
             'hello\x1b[mthere': 'hellothere',
-            'hello\x1b[01;34mthere': r'hello\textcolor{blue}{\textbf{there}}',
-            'hello\x1b[001;34mthere': r'hello\textcolor{blue}{\textbf{there}}',
-            '\x1b[1mhello\x1b[33mworld\x1b[0m': r'\textbf{hello}\textcolor{brown}{\textbf{world}}',
+            'hello\x1b[01;34mthere': r'hello\textcolor{ansi-blue-intense}{\textbf{there}}',
+            'hello\x1b[001;34mthere': r'hello\textcolor{ansi-blue-intense}{\textbf{there}}',
+            '\x1b[1mhello\x1b[33mworld\x1b[0m': r'\textbf{hello}\textcolor{ansi-yellow-intense}{\textbf{world}}',
         }
 
         for inval, outval in correct_outputs.items():
-            self.fuzzy_compare(outval, ansi2latex(inval), case_sensitive=True)
+            self.assertEqual(outval, ansi2latex(inval))
