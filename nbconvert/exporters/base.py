5eaddad912bf03e4e370a42f598c2bd2b0a3a519
michaelpacer <michaelpacer@gmail.com>
2016-09-19 22:21:08 -0700

    remove unused import statements

--- nbconvert/exporters/base.py
+++ nbconvert/exporters/base.py
@@ -3,8 +3,6 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 
-from functools import wraps
-from itertools import chain
 import warnings
 
 import entrypoints
