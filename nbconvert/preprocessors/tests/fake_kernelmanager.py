cf860db6aa8041f57667215b9f49dcf7913e789b
Nicholas Bollweg <nbollweg@continuum.io>
2016-11-08 16:36:50 -0800

    check that the client was initialized

--- nbconvert/preprocessors/tests/fake_kernelmanager.py
+++ nbconvert/preprocessors/tests/fake_kernelmanager.py
@@ -4,7 +4,8 @@ from jupyter_client.manager import KernelManager
 class FakeCustomKernelManager(KernelManager):
     expected_methods = {
         '__init__': 0,
-        'start_kernel': 0
+        'client': 0,
+        'start_kernel': 0,
     }
 
     def __init__(self, *args, **kwargs):
@@ -16,5 +17,12 @@ class FakeCustomKernelManager(KernelManager):
         self.log.info('FakeCustomKernelManager started a kernel')
         self.expected_methods['start_kernel'] += 1
         return super(FakeCustomKernelManager, self).start_kernel(
+            *args,
+            **kwargs)
+
+    def client(self, *args, **kwargs):
+        self.log.info('FakeCustomKernelManager created a client')
+        self.expected_methods['client'] += 1
+        return super(FakeCustomKernelManager, self).client(
             *args,
             **kwargs)
