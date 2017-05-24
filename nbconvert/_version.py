696b08cd9b59440aba6195c68cfc7b6d5ef765d8
mpacer <michaelpacer@gmail.com>
2017-05-23 19:44:36 -0700

    take into account the nonneed to pass in an integer at dev_seg

--- nbconvert/_version.py
+++ nbconvert/_version.py
@@ -29,18 +29,21 @@ def create_valid_version(release_info, epoch=None, pre_input='', dev_input=''):
         raise ValueError(pep440_err + "\n please fix your prerelease segment.")
     else:
         pre_seg = pre_input
-     
-    if dev_input!='' and (not dev_input.startswith('.') and dev_input.startswith('dev')):
+    
+    if dev_input=='':
+        dev_seg = dev_input
+    elif not dev_input.startswith('.') and dev_input.startswith('dev'):
         dev_seg = ''.join(['.', dev_input])
-    elif dev_input=='' or dev_input.startswith('.dev'):
+    elif dev_input.startswith('.dev'):
         dev_seg = dev_input
     elif dev_input!='':
-        print(dev_input)
         raise ValueError(pep440_err + "\n please fix your development segment.")
+    
+    if dev_input!='' and not any([dev_seg.endswith(str(n)) for n in range(10)]):
+        dev_seg = ''.join([dev_seg,'0'])
 
     out_version = ''.join([epoch_seg, release_seg, pre_seg, dev_seg])
 
-    print(out_version)
     if pep440.is_canonical(out_version):
         return out_version
     else:
@@ -50,7 +53,7 @@ def create_valid_version(release_info, epoch=None, pre_input='', dev_input=''):
 
 version_info = (5, 2, 0)
 pre_info = ''
-dev_info = '.dev0'
+dev_info = '.dev'
 __version__ = create_valid_version(version_info, pre_input=pre_info, dev_input=dev_info) 
 
 
