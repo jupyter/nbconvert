1db966646b1e28b157ff2fa86474256dc9ff781c
Danilo J. S. Bellini <danilo.bellini@gmail.com>
2017-12-04 03:36:35 -0200

    Add Markdown block lexer/parser for LaTeX blocks
    The block lexer/parser was splitting equations like this
    
    $$
    x
    =
    2
    $$
    
    So the inline lexer/parser was never seeing the whole equation, and
    it wasn't getting properly rendered. This fixes such breaking by
    adding a block-level lexer/parser to the LaTeX equations written as
    either $$...$$ or \\[...\\]
    
    The inline "block math" parsing code was kept as is, since the above
    equation could have been part of a paragraph like "$$x = 2$$" to keep
    the compatibility with Jupyter Notebook rendering engine (and because
    there's a test enforcing that behavior)

--- nbconvert/filters/markdown_mistune.py
+++ nbconvert/filters/markdown_mistune.py
@@ -21,12 +21,43 @@ from pygments.util import ClassNotFound
 from nbconvert.filters.strings import add_anchor
 
 
+class MathBlockGrammar(mistune.BlockGrammar):
+    block_math = re.compile(r"^\$\$(.*?)\$\$|^\\\\\[(.*?)\\\\\]", re.DOTALL)
+    latex_environment = re.compile(r"^\\begin\{([a-z]*\*?)\}(.*?)\\end\{\1\}",
+                                   re.DOTALL)
+
+
 class MathInlineGrammar(mistune.InlineGrammar):
     inline_math = re.compile(r"^\$(.+?)\$|^\\\\\((.+?)\\\\\)", re.DOTALL)
     block_math = re.compile(r"^\$\$(.*?)\$\$|^\\\\\[(.*?)\\\\\]", re.DOTALL)
     latex_environment = re.compile(r"^\\begin\{([a-z]*\*?)\}(.*?)\\end\{\1\}",
                                    re.DOTALL)
     text = re.compile(r'^[\s\S]+?(?=[\\<!\[_*`~$]|https?://| {2,}\n|$)')
+
+
+class MathBlockLexer(mistune.BlockLexer):
+    default_rules = (['block_math', 'latex_environment']
+                     + mistune.BlockLexer.default_rules)
+
+    def __init__(self, rules=None, **kwargs):
+        if rules is None:
+            rules = MathBlockGrammar()
+        super(MathBlockLexer, self).__init__(rules, **kwargs)
+
+    def parse_block_math(self, m):
+        """Add token for a $$math$$ block"""
+        self.tokens.append({
+            'type': 'block_math',
+            'text': m.group(1) or m.group(2)
+        })
+
+    def parse_latex_environment(self, m):
+        """Add token for a \begin{.} ...LaTeX stuff... \end{.} block"""
+        self.tokens.append({
+            'type': 'latex_environment',
+            'name': m.group(1),
+            'text': m.group(2)
+        })
 
 
 class MathInlineLexer(mistune.InlineLexer):
@@ -53,7 +84,16 @@ class MarkdownWithMath(mistune.Markdown):
     def __init__(self, renderer, **kwargs):
         if 'inline' not in kwargs:
             kwargs['inline'] = MathInlineLexer
+        if 'block' not in kwargs:
+            kwargs['block'] = MathBlockLexer
         super(MarkdownWithMath, self).__init__(renderer, **kwargs)
+
+    def output_block_math(self):
+        return self.renderer.block_math(self.token["text"])
+
+    def output_latex_environment(self):
+        return self.renderer.latex_environment(self.token["name"],
+                                               self.token["text"])
 
 
 class IPythonRenderer(mistune.Renderer):
