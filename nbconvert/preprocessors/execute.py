db9642b413cd8b347b471faee18355b6812a8fa5
Maarten Breddels <maartenbreddels@gmail.com>
2018-10-16 17:08:37 +0200

    fixes after rebases

--- nbconvert/preprocessors/execute.py
+++ nbconvert/preprocessors/execute.py
@@ -299,6 +299,8 @@ class ExecutePreprocessor(Preprocessor):
         self.nb = nb
         # clear display_id map
         self._display_id_map = {}
+        self.widget_state = {}
+        self.widget_buffers = {}
 
         if km is None:
             self.km, self.kc = self.start_new_kernel(cwd=path)
@@ -335,7 +337,6 @@ class ExecutePreprocessor(Preprocessor):
         Preprocess notebook executing each code cell.
 
         The input argument `nb` is modified in-place.
-        self.widget_buffers = {}
 
         Parameters
         ----------
@@ -362,6 +363,7 @@ class ExecutePreprocessor(Preprocessor):
             nb, resources = super(ExecutePreprocessor, self).preprocess(nb, resources)
             info_msg = self._wait_for_reply(self.kc.kernel_info())
             nb.metadata['language_info'] = info_msg['content']['language_info']
+            self.set_widgets_metadata()
 
         return nb, resources
 
