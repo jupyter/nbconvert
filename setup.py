ff40749ba756f33026de6f05090362c9f8c5c1c0
Thomas Kluyver <thomas@kluyver.me.uk>
2019-01-31 13:44:29 +0000

    Remove duplicated long description
    The code just above reads the README

--- setup.py
+++ setup.py
@@ -160,19 +160,6 @@ with open(pjoin(here, name, '_version.py')) as f:
 with io.open(pjoin(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-long_description="""
-
-Nbconvert is a Command Line tool and Python library and API to process and
-convert Jupyter notebook into a variety of other formats.
-
-Using nbconvert enables:
-
-  - presentation of information in familiar formats, such as PDF.
-  - publishing of research using LaTeX and opens the door for embedding notebooks in papers.
-  - collaboration with others who may not use the notebook in their work.
-  - sharing contents with many people via the web using HTML.
-"""
-
 setup_args = dict(
     name            = name,
     description     = "Converting Jupyter Notebooks",
