654d6a395650e403b41f16a235cf29f0fdb29a92
Maarten Breddels <maartenbreddels@gmail.com>
2019-04-05 15:27:22 +0200

    implement attachment using markdown2html filter

--- nbconvert/exporters/html.py
+++ nbconvert/exporters/html.py
@@ -10,12 +10,12 @@ import re
 
 from traitlets import default, Unicode
 from traitlets.config import Config
+from jinja2 import contextfilter
 
 from nbconvert.filters.highlight import Highlight2HTML
 from nbconvert.filters.markdown_mistune import IPythonRenderer, MarkdownWithMath
 
 from .templateexporter import TemplateExporter
-
 
 class HTMLExporter(TemplateExporter):
     """
@@ -68,9 +68,12 @@ class HTMLExporter(TemplateExporter):
         c.merge(super(HTMLExporter,self).default_config)
         return c
 
-    def markdown2html(self, source):
+    @contextfilter
+    def markdown2html(self, context, source):
         """Markdown to HTML filter respecting the anchor_link_text setting"""
-        renderer = IPythonRenderer(escape=False,
+        cell = context['cell']
+        attachments = cell.get('attachments', {})
+        renderer = IPythonRenderer(escape=False, attachments=attachments,
                                    anchor_link_text=self.anchor_link_text)
         return MarkdownWithMath(renderer=renderer).render(source)
 
@@ -79,44 +82,9 @@ class HTMLExporter(TemplateExporter):
             yield pair
         yield ('markdown2html', self.markdown2html)
 
-    def process_attachments(self, nb, output):
-        for cell in nb.cells:
-            if 'attachments' in cell:
-                for key, attachment in cell['attachments'].items():
-                    for att_type, att in attachment.items():
-                        output = output.replace(
-                            'attachment:{}'.format(key),
-                            'data:' + att_type + ';base64, ' + attachment[att_type])
-        return output
-
-    def _make_attachments_globally_unique(self, nb):
-        nb = copy.deepcopy(nb)
-        for cell_index, cell in enumerate(nb.cells):
-            if 'attachments' in cell:
-                # attachment with names being unique only within the cell not globally
-                local_attachments = cell['attachments'].copy()
-                # we will replace them with a globally unique name
-                cell['attachments'] = {}
-                for key, attachment in local_attachments.items():
-                    unique_key = 'globally_unique_%i_%s' % (cell_index, key)
-                    cell['attachments'][unique_key] = attachment
-                    if 'source' in cell:
-                        # the reference to the attachment should also be renamed
-                        local_ref = r'](attachment:%s)' % key
-                        global_ref = r'](attachment:%s)' % unique_key
-                        cell['source'] = re.sub(local_ref, global_ref, cell['source'])
-                        cell['source'] = cell['source'].replace(local_ref, global_ref)
-        return nb
-
-
-
     def from_notebook_node(self, nb, resources=None, **kw):
-        self.nb = nb = self._make_attachments_globally_unique(nb)
         langinfo = nb.metadata.get('language_info', {})
         lexer = langinfo.get('pygments_lexer', langinfo.get('name', None))
         highlight_code = self.filters.get('highlight_code', Highlight2HTML(pygments_lexer=lexer, parent=self))
         self.register_filter('highlight_code', highlight_code)
-        
-        output, resources = super(HTMLExporter, self).from_notebook_node(nb, resources, **kw)
-        att_output = self.process_attachments(nb, output)
-        return att_output, resources
+        return super(HTMLExporter, self).from_notebook_node(nb, resources, **kw)
