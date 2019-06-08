1c0791eaed1213c43ce45053bd233aa5dfd2b983
Tyler Makaro <t-makaro@users.noreply.github.com>
2019-06-08 09:55:59 -0700

    fix python 2.7

--- nbconvert/utils/commands.py
+++ nbconvert/utils/commands.py
@@ -3,5 +3,6 @@ from ..exporters import TemplateExporter, HTMLExporter, LatexExporter
 
 def ensure_template_dirs_exist():
     for exporter in [TemplateExporter(), HTMLExporter(), LatexExporter()]:
-        for path in [*exporter.template_data_paths, exporter.user_config_template_path]:
-                ensure_dir_exists(path, mode=0o700)
+        paths = exporter.template_data_paths+[exporter.user_config_template_path]
+        for path in paths:
+            ensure_dir_exists(path, mode=0o700)
