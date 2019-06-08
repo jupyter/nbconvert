bcde03ac3622ca6f862b8558d75f37003cd20bcb
Tyler Makaro <t-makaro@users.noreply.github.com>
2019-06-08 10:10:10 -0700

    attempt to create directories, but don't force

--- nbconvert/utils/commands.py
+++ nbconvert/utils/commands.py
@@ -5,4 +5,7 @@ def ensure_template_dirs_exist():
     for exporter in [TemplateExporter(), HTMLExporter(), LatexExporter()]:
         paths = exporter.template_data_paths+[exporter.user_config_template_path]
         for path in paths:
-            ensure_dir_exists(path, mode=0o700)
+            try:
+                ensure_dir_exists(path, mode=0o700)
+            except OSError:
+                pass
