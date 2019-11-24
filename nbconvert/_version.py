9d3e38a9f243d92b9089a8f47b2f17ba4249a68b
Matthew Seal <mseal007@gmail.com>
2019-11-24 10:58:53 -0800

    alpha 6.0.0a0 release

--- nbconvert/_version.py
+++ nbconvert/_version.py
@@ -1,6 +1,6 @@
 version_info = (6, 0, 0)
-pre_info = ''
-dev_info = '.dev'
+pre_info = 'a0'
+dev_info = ''
 
 def create_valid_version(release_info, epoch=None, pre_input='', dev_input=''):
     '''
