1a35cdae28e9c88f0f732d92ba82cba722836aea
David Brochart <david.brochart@gmail.com>
2020-06-11 20:21:33 +0200

    Add tests

--- nbconvert/tests/test_nbconvertapp.py
+++ nbconvert/tests/test_nbconvertapp.py
@@ -131,6 +131,27 @@ class TestNbConvertApp(TestsBase):
             )
             assert os.path.isfile('notebook with spaces.pdf')
 
+    def test_webpdf_without_chromium(self):
+        """
+        Generate PDFs if chromium not present?
+        """
+        with self.create_temp_cwd(['notebook2.ipynb']):
+            with pytest.raises(OSError):
+                self.nbconvert('--to webpdf '
+                               '"notebook2"'
+                )
+
+    def test_webpdf_with_chromium(self):
+        """
+        Generate PDFs if chromium allowed to be downloaded?
+        """
+        with self.create_temp_cwd(['notebook2.ipynb']):
+            self.nbconvert('--to webpdf '
+                           '--allow-chromium-download '
+                           '"notebook2"'
+            )
+            assert os.path.isfile('notebook2.pdf')
+
     @onlyif_cmds_exist('pandoc', 'xelatex')
     def test_pdf(self):
         """
