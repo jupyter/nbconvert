be588a2840d4997f8473fedf5cf1dc69eb815b84
Matthias Geier <Matthias.Geier@gmail.com>
2020-08-25 19:32:45 +0200

    DOC: use pygments_style provided by HTML theme (#1351)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -115,7 +115,7 @@ default_role = 'any'
 #show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+#pygments_style = 'sphinx'
 
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
