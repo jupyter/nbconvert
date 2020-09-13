57c716ca9fec2ccc88ea11feaaafaa3d163134d0
Matthew Seal <mseal007@gmail.com>
2020-09-12 17:36:57 -0700

    Addressed feedback

--- nbconvert/preprocessors/execute.py
+++ nbconvert/preprocessors/execute.py
@@ -44,6 +44,17 @@ class ExecutePreprocessor(Preprocessor, NotebookClient):
 
         The input argument *nb* is modified in-place.
 
+        Note that this function recalls NotebookClient.__init__, which may look wrong.
+        However since the preprocess call acts line an init on exeuction state it's expected.
+        Therefore, we need to capture it here again to properly reset because traitlet 
+        assignments are not passed. There is a risk if traitlets apply any side effects for
+        dual init.
+        The risk should be manageable, and this approach minimizes side-effects relative
+        to other alternatives.
+
+        One alternative but rejected implementation would be to copy the client's init internals
+        which has already gotten out of sync with nbclient 0.5 release before nbcovnert 6.0 released.
+
         Parameters
         ----------
         nb : NotebookNode
@@ -63,12 +74,6 @@ class ExecutePreprocessor(Preprocessor, NotebookClient):
         resources : dictionary
             Additional resources used in the conversion process.
         """
-        # Although this solution may look wrong, it considers that preprocess acts like an init on execution state.
-        # Therefore, we need to capture it here again to properly reset because traitlet assignments are
-        # not passed. There is a risk if traitlets apply any side effects for dual init.
-        # The risk should be manageable, and this approach minimizes side-effects
-        # relative to other alternatives. One alternative but rejected implementation would be to copy the client's init internals
-        # which has already gotten out of sync with nbclient 0.5 release.
         NotebookClient.__init__(self, nb, km)
         self._check_assign_resources(resources)
         self.execute()
@@ -83,7 +88,7 @@ class ExecutePreprocessor(Preprocessor, NotebookClient):
         """
         Executes a single code cell.
 
-        To execute all cells see :meth:`execute`.
+        Overwrites NotebookClient's version of this method to allow for preprocess_cell calls.
 
         Parameters
         ----------
@@ -136,5 +141,6 @@ class ExecutePreprocessor(Preprocessor, NotebookClient):
             Index of the cell being processed
         """
         self._check_assign_resources(resources)
+        # Because nbclient is an async library, we need to wrap the parent async call to generate a syncronous version.
         cell = run_sync(NotebookClient.async_execute_cell)(self, cell, index, store_history=self.store_history)
         return cell, self.resources
