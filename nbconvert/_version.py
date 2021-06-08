2cf87d38312b157f7dd868d11fadc38e03b20405
Matthew Seal <mseal007@gmail.com>
2021-06-07 23:34:59 -0700

    release 6.1.0rc0

--- nbconvert/_version.py
+++ nbconvert/_version.py
@@ -1,6 +1,6 @@
-version_info = (6, 0, 8)
-pre_info = ''
-dev_info = '.dev'
+version_info = (6, 1, 0)
+pre_info = 'rc0'
+dev_info = ''
 
 def create_valid_version(release_info, epoch=None, pre_input='', dev_input=''):
     '''
