c11b242697d39ee60f2b0bc221044cdfb77a08cd
Matthew Seal <mseal007@gmail.com>
2021-06-22 16:42:55 -0700

    Added dev suffix back to version

--- nbconvert/_version.py
+++ nbconvert/_version.py
@@ -1,6 +1,6 @@
-version_info = (6, 1, 0)
+version_info = (6, 1, 1)
 pre_info = ''
-dev_info = ''
+dev_info = '.dev'
 
 def create_valid_version(release_info, epoch=None, pre_input='', dev_input=''):
     '''
