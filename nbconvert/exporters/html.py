163797398b5d38fc40dd5e7588eacb19cf495014
Youness Bennani <ybennani@bloomberg.net>
2021-08-11 15:09:04 -0700

    Add the ability to cutomize the full widget URL

--- nbconvert/exporters/html.py
+++ nbconvert/exporters/html.py
@@ -59,6 +59,8 @@ class HTMLExporter(TemplateExporter):
         help="URL base for Jupyter widgets"
     ).tag(config=True)
 
+    widgets_url = Unicode("", help="Full URL for Jupyter widgets").tag(config=True)
+
     html_manager_semver_range = Unicode(
         "*",
         help="Semver range for Jupyter widgets HTML manager"
@@ -171,5 +173,6 @@ class HTMLExporter(TemplateExporter):
         resources['require_js_url'] = self.require_js_url
         resources['jquery_url'] = self.jquery_url
         resources['jupyter_widgets_base_url'] = self.jupyter_widgets_base_url
+        resources['widgets_url'] = self.widgets_url
         resources['html_manager_semver_range'] = self.html_manager_semver_range
         return resources
