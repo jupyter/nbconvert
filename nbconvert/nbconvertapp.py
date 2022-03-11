f6bd3df9887c250580a68c2c63231caebdf7debe
rgs258 <rgs258@users.noreply.github.com>
2022-03-11 11:57:50 -0500

    Remove ipython genutils (#1727)

--- nbconvert/nbconvertapp.py
+++ nbconvert/nbconvertapp.py
@@ -15,9 +15,9 @@ import os
 import glob
 import asyncio
 from textwrap import fill, dedent
-from ipython_genutils.text import indent
 
 from jupyter_core.application import JupyterApp, base_aliases, base_flags
+from nbconvert.utils.text import indent
 from traitlets.config import catch_config_error, Configurable
 from traitlets import (
     Unicode, List, Instance, DottedObjectName, Type, Bool,
