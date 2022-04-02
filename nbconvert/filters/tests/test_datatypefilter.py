a8b58e827c7d907523ea0f9cf64c67ad302d5712
Steven Silvester <steven.silvester@ieee.org>
2022-04-01 22:13:08 -0500

    add pytest settings and handle warnings

--- nbconvert/filters/tests/test_datatypefilter.py
+++ nbconvert/filters/tests/test_datatypefilter.py
@@ -2,7 +2,7 @@
 
 # Copyright (c) IPython Development Team.
 # Distributed under the terms of the Modified BSD License.
-
+import pytest
 from ...tests.base import TestsBase
 from ..datatypefilter import DataTypeFilter
 
@@ -19,9 +19,12 @@ class TestDataTypeFilter(TestsBase):
         filter = DataTypeFilter()
         assert "image/png" in filter({"hair":"1", "water":2, "image/png":3, "rock":4.0})
         assert "application/pdf" in filter({"application/pdf":"file_path", "hair":2, "water":"yay", "png":'not a png', "rock":'is a rock'})
-        self.assertEqual(filter({"hair":"this is not", "water":"going to return anything", "rock":"or is it"}), [])
+
+        with pytest.warns(UserWarning):
+            self.assertEqual(filter({"hair":"this is not", "water":"going to return anything", "rock":"or is it"}), [])
 
     def test_null(self):
         """Will the DataTypeFilter fail if no types are passed in?"""
         filter = DataTypeFilter()
-        self.assertEqual(filter({}), [])
+        with pytest.warns(UserWarning):
+            self.assertEqual(filter({}), [])
