60af6d897c083444586829c636f278d84ae81962
Tiago de Paula <tiagodepalves@gmail.com>
2023-06-16 17:15:05 -0300

    Update to Mistune v3 (#1820)

--- nbconvert/filters/markdown.py
+++ nbconvert/filters/markdown.py
@@ -11,13 +11,14 @@ import re
 
 try:
     from .markdown_mistune import markdown2html_mistune
+
 except ImportError as e:
-    # store in variable for Python 3
     _mistune_import_error = e
 
-    def markdown2html_mistune(source):
+    def markdown2html_mistune(source: str) -> str:
         """mistune is unavailable, raise ImportError"""
-        raise ImportError("markdown2html requires mistune: %s" % _mistune_import_error)
+        msg = f"markdown2html requires mistune: {_mistune_import_error}"
+        raise ImportError(msg)
 
 
 from .pandoc import convert_pandoc
