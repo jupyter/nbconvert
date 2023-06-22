0c4ca29eca1fe57c7b4c6e9b3ee5dffb2e78d24a
Steven Silvester <steven.silvester@ieee.org>
2023-06-22 10:14:45 -0500

    debug

--- nbconvert/conftest.py
+++ nbconvert/conftest.py
@@ -1,6 +1,9 @@
 """pytest configuration."""
 import asyncio
+import faulthandler
 import os
+
+faulthandler.enable()
 
 if os.name == "nt":
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())  # type:ignore
