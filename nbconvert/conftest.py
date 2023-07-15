d47f6df46ca99801bde2994ed70175c99ce727f2
Steven Silvester <steven.silvester@ieee.org>
2023-07-15 00:26:11 -0500

    use pytest-faulthandler

--- nbconvert/conftest.py
+++ nbconvert/conftest.py
@@ -1,9 +1,6 @@
 """pytest configuration."""
 import asyncio
-import faulthandler
 import os
-
-faulthandler.enable()
 
 if os.name == "nt":
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())  # type:ignore
