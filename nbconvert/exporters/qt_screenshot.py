55679e680fdd6199168c2d5eaed8c8f2509814fb
Steven Silvester <steven.silvester@ieee.org>
2023-10-14 19:55:12 -0500

    fixup

--- nbconvert/exporters/qt_screenshot.py
+++ nbconvert/exporters/qt_screenshot.py
@@ -4,12 +4,10 @@ import os
 try:
     from PyQt5 import QtCore  # type:ignore[import-not-found]
     from PyQt5.QtGui import QPageLayout, QPageSize  # type:ignore[import-not-found]
-    from PyQt5.QtWebEngineWidgets import (
+    from PyQt5.QtWebEngineWidgets import (  # type:ignore[import-not-found]
         QWebEngineSettings,
         QWebEngineView,
     )
-
-    # type:ignore[import-not-found]
     from PyQt5.QtWidgets import QApplication  # type:ignore[import-not-found]
 
     QT_INSTALLED = True
