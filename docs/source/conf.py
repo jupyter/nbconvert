858d63400123ea74f276ddc78f2526a63bcd36b5
Nicholas Bollweg <nick.bollweg@gmail.com>
2023-10-28 15:34:42 -0500

    Update to mermaid 10.6.0, docs keyboard navigation (#2058)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -155,7 +155,10 @@ html_theme = "pydata_sphinx_theme"
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-# html_theme_options = {}
+html_theme_options = {
+    # prefer browser defaults over custom JS keyboard event handlers
+    "navigation_with_keys": False,
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
