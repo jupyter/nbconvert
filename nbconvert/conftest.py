513c6b9c3541f484c1bfa275fc205c7fd3fdccf1
Steven Silvester <steven.silvester@ieee.org>
2023-12-26 09:24:57 -0600

    Isolate the test

--- nbconvert/conftest.py
+++ nbconvert/conftest.py
@@ -1,8 +1,17 @@
 """pytest configuration."""
 import asyncio
+import gc
 import os
+
+import pytest
 
 if os.name == "nt":
     asyncio.set_event_loop_policy(
         asyncio.WindowsSelectorEventLoopPolicy()  # type:ignore[attr-defined]
     )
+
+
+@pytest.fixture(autouse=True)
+def isolate():  # noqa: PT004
+    yield
+    gc.collect()
