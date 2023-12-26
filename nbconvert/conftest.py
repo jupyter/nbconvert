0055e5db503bc0cc9de1d0c3e73afdad8dcaa7ac
Steven Silvester <steven.silvester@ieee.org>
2023-12-26 09:53:08 -0600

    ensure playwright is stopped

--- nbconvert/conftest.py
+++ nbconvert/conftest.py
@@ -1,17 +1,8 @@
 """pytest configuration."""
 import asyncio
-import gc
 import os
-
-import pytest
 
 if os.name == "nt":
     asyncio.set_event_loop_policy(
         asyncio.WindowsSelectorEventLoopPolicy()  # type:ignore[attr-defined]
     )
-
-
-@pytest.fixture(autouse=True)
-def isolate():  # noqa: PT004
-    yield
-    gc.collect()
