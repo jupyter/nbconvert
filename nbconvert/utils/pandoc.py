52e583168794663eaeabbdadeef1dae8eba188ff
Steven Silvester <steven.silvester@ieee.org>
2024-03-21 12:22:33 -0500

    Add Compatibility with Pandoc 2.9.2 (#2128)

--- nbconvert/utils/pandoc.py
+++ nbconvert/utils/pandoc.py
@@ -13,7 +13,7 @@ from nbconvert.utils.version import check_version
 
 from .exceptions import ConversionException
 
-_minimal_version = "2.14.2"
+_minimal_version = "2.9.2"
 _maximal_version = "4.0.0"
 
 
