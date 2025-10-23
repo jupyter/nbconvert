00f8cfd194db47d732b9be9b119b1e066fd44527
Piotr Masłowski <piotr@maslowski.xyz>
2025-10-23 05:07:30 +0200

    preprocessors: Improve clearmetadata help/docstrings

--- nbconvert/preprocessors/clearmetadata.py
+++ nbconvert/preprocessors/clearmetadata.py
@@ -1,4 +1,4 @@
-"""Module containing a preprocessor that removes metadata from code cells"""
+"""Module containing a preprocessor that removes notebook or code cell metadata"""
 
 # Copyright (c) IPython Development Team.
 # Distributed under the terms of the Modified BSD License.
@@ -10,7 +10,7 @@ from .base import Preprocessor
 
 class ClearMetadataPreprocessor(Preprocessor):
     """
-    Removes all the metadata from all code cells in a notebook.
+    Removes global or cell-level metadata from a notebook.
     """
 
     clear_cell_metadata = Bool(
@@ -24,16 +24,16 @@ class ClearMetadataPreprocessor(Preprocessor):
     preserve_nb_metadata_mask = Set(
         [("language_info", "name")],
         help=(
-            "Indicates the key paths to preserve when deleting metadata "
-            "across both cells and notebook metadata fields. Tuples of "
-            "keys can be passed to preserved specific nested values"
+            "Indicates the key paths to preserve when deleting notebook "
+            "metadata fields. Tuples of keys can be passed to preserve "
+            "specific nested values."
         ),
     ).tag(config=True)
     preserve_cell_metadata_mask = Set(
         help=(
             "Indicates the key paths to preserve when deleting metadata "
-            "across both cells and notebook metadata fields. Tuples of "
-            "keys can be passed to preserved specific nested values"
+            "across all code cells in a notebook. Tuples of keys can be "
+            "passed to preserve specific nested values."
         )
     ).tag(config=True)
 
@@ -73,7 +73,7 @@ class ClearMetadataPreprocessor(Preprocessor):
 
     def preprocess_cell(self, cell, resources, cell_index):
         """
-        All the code cells are returned with an empty metadata field.
+        All the code cells are returned with a trimmed down metadata field.
         """
         if self.clear_cell_metadata and cell.cell_type == "code":  # noqa: SIM102
             # Remove metadata
